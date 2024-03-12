abf41de9a46f9c98ecbf56d22ebf6e14584027e1
sergiobni <bni.sergio@gmail.com>
2024-03-12 13:26:53 +0100

    tailwind flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Sergio</div>;
+  return <div className="hidden text-sm font-semibold md:block">Sergio</div>;
 }
 
 export default Username;
