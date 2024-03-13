76929345f0e2e76efc396c522fd4d8da66bc716b
sergiobni <bni.sergio@gmail.com>
2024-03-13 12:21:34 +0100

    styling the cart

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,13 +1,13 @@
-import { useLoaderData } from 'react-router-dom';
-import { getMenu } from '../../services/apiRestaurant';
-import MenuItem from './MenuItem';
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData(); //Data coming from the loader of app.jsx
   console.log(menu);
   return (
-    <ul>
-      {menu.map(pizza => (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
